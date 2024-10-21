b3d01682dcfb1d2053562734119302b49c5aaa4e
marco672 <belmontemolino.pt@gmail.com>
2024-10-21 11:37:21 -0100

    commit 12

---
+++ js/script.js
@@ -0,0 +1,3 @@
+document.getElementById('emailButton').addEventListener('click', function() {
+    window.location.href = "mailto: marcobelmonte474@gmail.com?subject=Contact via Website&body=Hi, Marco Belmonte!";
+});
